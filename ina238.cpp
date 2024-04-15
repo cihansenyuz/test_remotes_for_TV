ffb8c3911d9a07556c6d77ffad7a9064ddfa5218
cihansenyuz <cihansenyuz@hotmail.com>
2024-04-15 14:49:24 +0300

    ina238.h and ina238.cpp created

---
+++ ina238.cpp
@@ -0,0 +1,79 @@
+# include "ina238.h"
+
+Ina238::Ina238(uint8_t addr, int busNum) {
+    fd = wiringPiI2CSetup(addr);
+    m_bus = busNum;
+    m_addr = addr;
+    m_currentLsb = 0;
+
+    /*
+    CONFIG reg: 1R00 0000 00R0 RRRR
+                                ^ADCRANGE selection
+                    ^--------^ADC conversiton delay
+                ^system reset
+    */
+    // do a system reset
+    writeWordData(fd, CONFIG, 0x8000);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    deviceID();
+
+    uint16_t word = getWordData(ADC_CONFIG);
+    word &= 0xFF8;     // clear bits 2-0 & 15-12
+    word |= 0x04;      // set ADC averaging to 128
+    word |= 0x7000;     // set ADC mode triggered
+    writeWordData(fd, ADC_CONFIG, word);
+}
+
+uint16_t Ina238::reverseWord(uint16_t oldWord) {
+    uint16_t newWord = oldWord & 0x00FF;    // mask MSByte of oldWord    
+    newWord = newWord << 8;                // shift LSByte of newWord to MSByte   
+    newWord |= (oldWord >> 8);             // shift MSByte of oldWord to LSByte and OR it to newWord
+    return newWord;
+}
+
+uint16_t Ina238::getWordData(uint8_t reg) {
+    uint16_t word = static_cast<uint16_t>(reverseWord(wiringPiI2CReadReg16(fd, reg)));
+    return word;
+}
+
+void Ina238::writeWordData(int fd, uint8_t reg, uint16_t data){
+    wiringPiI2CWriteReg16(fd, reg, reverseWord(data));
+}
+
+void Ina238::setShuntCal(double res, double maxCur) {
+    m_currentLsb = maxCur / 32768.0f;
+    double factor = 1.0f;
+    while (m_currentLsb < 1.0f) {
+        m_currentLsb *= 10.0f;
+        factor *= 10.0f;
+    }
+    m_currentLsb = 250.0f / factor;
+    uint16_t shuntCalValue = static_cast<uint16_t>(std::round(819200000.0f * m_currentLsb * res));
+    writeWordData(fd, SHUNT_CAL, shuntCalValue);
+}
+
+void Ina238::deviceID() {
+    uint16_t word = getWordData(DEVICE_ID);
+    std::cout << "Device ID: 0x" << std::hex << word << " is ready" << std::endl;
+}
+
+void Ina238::temperature() {
+    uint16_t word = getWordData(DIETEMP);
+    float temp = static_cast<float>(word >> 4) * 0.125f; // shift 4 bits cause bits 0-3 are reserved
+    std::cout << "Die Temp: " << temp << " C" << std::endl;
+}
+
+float Ina238::voltage() {
+    uint16_t word = getWordData(VBUS);
+    float batVoltage = static_cast<float>(word) * 0.003125f;
+    std::cout << "Bus Voltage: " << batVoltage << " V" << std::endl;
+    return batVoltage;
+}
+
+float Ina238::current() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    uint16_t word = getWordData(CURRENT);
+    float current = static_cast<float>(word) * m_currentLsb * 1000.0f;
+    std::cout << "Current: " << current << " mA" << std::endl;
+    return current;
+}
---
+++ ina238.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <wiringPiI2C.h>
+#include <chrono>
+#include <thread>
+
+#define DEVICE_ID 0x3F        // slave device ID register
+#define CONFIG 0x00           // Configuration (CONFIG) Register
+#define ADC_CONFIG 0x01       // ADC Configuration (ADC_CONFIG) Register
+#define SHUNT_CAL 0x02        // Shunt calibration
+#define VSHUNT 0x04           // Shunt Voltage Measurement
+#define VBUS 0x05             // Bus Voltage Measurement
+#define DIETEMP 0x06          // Temperature Measurement (DIETEMP) Register
+#define CURRENT 0x07          // Current result
+#define POWER 0x08            // Power result
+
+class Ina238 {
+public:
+    Ina238(uint8_t addr, int busNum);
+    ~Ina238(){}
+    uint16_t getWordData(uint8_t reg);
+    void writeWordData(int fd, uint8_t reg, uint16_t data);
+    void setShuntCal(double res, double maxCur);
+    uint16_t reverseWord(uint16_t oldWord);
+    void deviceID();
+    void temperature();
+    float voltage();
+    float current();
+private:
+    int fd;
+    int m_bus;
+    uint8_t m_addr;
+    double m_currentLsb;
+};
