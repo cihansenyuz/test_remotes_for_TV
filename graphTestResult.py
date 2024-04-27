7a626ebe014e436003784be4c7f0bf5aab12d288
Hamdi <86834533+hdnecli@users.noreply.github.com>
2024-04-27 16:25:22 +0300

    Update graphTestResult.py
    Graph file is updated.

--- graphTestResult.py
+++ graphTestResult.py
@@ -4,42 +4,42 @@ import numpy as np
 import sys 
 
 xLabels = []
+testValues = []
 
-def read_data():
-    global xlabel
+def openFileAndConfigureData(xLabels_type = int, testValues_type = float):
     with open("testResults.txt", "r") as file:
         lines = file.readlines()
-    testValues = []
     for line in lines:
         parts = line.strip().split(": ")
-        if "--sc" in sys.argv:
-            xLabels.append(float(parts[0][4:]))
-            testValues.append(float(parts[1]))
-            xlabel = "Time (h,m)"
-        elif "--sibc" in sys.argv:
-            xLabels.append(int(parts[0][4:]))
-            testValues.append(float(parts[1]))
-            xlabel = "Test Number"
-    return testValues
+        xLabels.append(xLabels_type(parts[0][4:]))
+        testValues.append(testValues_type(parts[1]))
+    num_data_points = len(testValues)
+    x_values = np.linspace(xLabels[0], xLabels[-1], num=num_data_points)
+    return x_values
 
-#def update_graph(frame):
-testValues = read_data()
-num_data_points = len(testValues)
-x_values = np.linspace(xLabels[0], xLabels[-1], num=num_data_points)
+def readDataSibc():
+    x_values = openFileAndConfigureData()
+    xlabel = "Test Number"
+    return x_values, xlabel
+
+def readDataSc():
+    x_values = openFileAndConfigureData(xLabels_type = float)
+    xlabel = "Time (h,m)"
+    plt.xticks(x_values, ['{:.2f}'.format(val) for val in x_values])  
+    return x_values, xlabel
+
 plt.cla()
+if "--sc" in sys.argv:
+    x_values, xlabel = readDataSc()
+elif "--sibc" in sys.argv:
+    read_data, x_values, xlabel = readDataSibc()
 plt.plot(x_values, testValues[:len(x_values)], color='#1f77b4', linewidth=2, marker='o', markersize=6, markerfacecolor='#ff7f0e', markeredgewidth=1, markeredgecolor='#ff7f0e')
 plt.title('Test Results', fontsize=16, fontweight='bold')
-if "--sc" in sys.argv:
-    plt.xticks(x_values, ['{:.2f}'.format(val) for val in x_values])  
 plt.xlabel(xlabel, fontsize=14)
 plt.ylabel('Voltage', fontsize=14)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 plt.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
-
-#plt.figure(figsize=(10, 6))
-
-#ani = animation.FuncAnimation(plt.gcf(), update_graph, frames=len(read_data()), interval=1000)  
 
 plt.tight_layout()
 plt.show()
