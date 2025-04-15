89f9ec5cf53a8c1f75febbc7d7179220a319e8cb
cihansenyuz <cihansenyuz@hotmail.com>
2025-04-15 17:14:25 +0300

    update cmake and main for the new test

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include "inc/solarlessIrButtonClick.hpp"
 #elif defined(TEST_FILE_SOLARCHARGE)
 #include "inc/solarCharge.hpp"
+#elif defined(TEST_FILE_PUSHANDMEAS)
+#include "inc/pushAndMeas.hpp"
 #endif
 
 int main(int argc, char **argv) {
@@ -13,6 +15,9 @@ int main(int argc, char **argv) {
 #elif defined(TEST_FILE_SOLARCHARGE)
     SolarCharge solar_charge_test;
     solar_charge_test.runTest();
+#elif defined(TEST_FILE_PUSHANDMEAS)
+    PushAndMeas push_and_meas_test;
+    push_and_meas_test.runTest();
 #else
     std::cerr << "Geçerli bir test dosyası belirtilmedi." << std::endl;
     return EXIT_FAILURE;
