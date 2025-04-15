326ceb217dadafaaf042bd14669c83291e9c9d05
cihansenyuz <cihansenyuz@hotmail.com>
2025-04-15 16:47:19 +0300

    update cmake build and main.cpp for test options

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#include "inc/solarlessIrButtonClick.hpp"
 
-int main(int argc, char **argv){
+#ifdef TEST_FILE_SOLARLESSIRBUTTONCLICK
+#include "inc/solarlessIrButtonClick.hpp"
+#elif defined(TEST_FILE_SOLARCHARGE)
+#include "inc/solarCharge.hpp"
+#endif
+
+int main(int argc, char **argv) {
+#ifdef TEST_FILE_SOLARLESSIRBUTTONCLICK
     SolarlessIrButtonClick button_click_test;
     button_click_test.runTest();
+#elif defined(TEST_FILE_SOLARCHARGE)
+    SolarCharge solar_charge_test;
+    solar_charge_test.runTest();
+#else
+    std::cerr << "Geçerli bir test dosyası belirtilmedi." << std::endl;
+    return EXIT_FAILURE;
+#endif
 
     return EXIT_SUCCESS;
 }
