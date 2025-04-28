a80092c4d727a582363b388676322a8aaef7cf4f
cihansenyuz <cihansenyuz@hotmail.com>
2025-04-28 15:22:50 +0300

    update source files in cmake, remove unnecessary defines

--- main.cpp
+++ main.cpp
@@ -2,36 +2,10 @@
 #include <QApplication>
 #include "inc/gui/mainwindow.hpp"
 
-#ifdef TEST_FILE_IRSVT
-#include "inc/irSignalValidationTest.hpp"
-#elif defined(TEST_FILE_BCMT)
-#include "inc/batteryChargeMonitoringTest.hpp"
-#elif defined(TEST_FILE_BVABT)
-#include "inc/batteryVoltageAndButtonTest.hpp"
-#endif
-
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
     MainWindow main_window;
     main_window.show();
-    
-#ifdef TEST_FILE_IRSVT
-    IrSignalValidationTest test;
-    QObject::connect(&test, &IrSignalValidationTest::finished, &app, &QCoreApplication::quit, Qt::QueuedConnection);
 
-#elif defined(TEST_FILE_BCMT)
-    BatteryChargeMonitoringTest test;
-    QObject::connect(&test, &BatteryChargeMonitoringTest::finished, &app, &QCoreApplication::quit, Qt::QueuedConnection);
-    
-#elif defined(TEST_FILE_BVABT)
-    BatteryVoltageAndButtonTest test;
-    QObject::connect(&test, &BatteryVoltageAndButtonTest::finished, &app, &QCoreApplication::quit, Qt::QueuedConnection);
-    
-#else
-    std::cerr << "Geçerli bir test dosyası belirtilmedi." << std::endl;
-    return EXIT_FAILURE;
-#endif
-
-    //test.runTest();
     return app.exec();
 }
