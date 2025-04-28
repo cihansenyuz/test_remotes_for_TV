a11f9738b786e54d9376c105fc09a46bd2a2ade5
cihansenyuz <cihansenyuz@hotmail.com>
2025-04-28 20:12:37 +0300

    implement business logic in dialog of IRSVT

--- src/gui/IRSVTsettingsdialog.cpp
+++ src/gui/IRSVTsettingsdialog.cpp
@@ -1,5 +1,6 @@
 #include "../../inc/gui/IRSVTsettingsdialog.hpp"
 #include "../../res/ui/ui_IRSVTsettingsdialog.h"
+#include "../../inc/testconfig.hpp"
 
 IRSVTSettingsDialog::IRSVTSettingsDialog(QWidget *parent)
     : QDialog(parent)
@@ -19,7 +20,15 @@ IRSVTSettingsDialog::~IRSVTSettingsDialog()
 }
 
 void IRSVTSettingsDialog::onSaveButtonClicked(){
-    // save test settings
+    TestSettings::getInstance().IR.dataSize = ui->dataSizeSpinBox->getValue();
+    TestSettings::getInstance().IR.minHeaderDuration = ui->minHeaderDurationSpinBox->getValue();
+    TestSettings::getInstance().IR.maxHeaderDuration = ui->maxHeaderDurationSpinBox->getValue();
+
+    TestSettings::getInstance().IRSVT.totalTestCount = ui->totalTestCountSpinBox->getValue();
+    TestSettings::getInstance().IRSVT.testQuantityToMeasure = ui->testQuantityToMeasureSpinBox->getValue();
+    TestSettings::getInstance().IRSVT.measurementQuantityToSave = ui->measurementQuantityToSaveSpinBox->getValue();
+    TestSettings::getInstance().IRSVT.totalErrorToFailTest = ui->totalErrorToFailTestSpinBox->getValue();
+
     accept();
 }
 
